6b79c6fc771d514b5f538c2663ee883ac2daba6c
dennis-lai96 <dennis.lai96@gmail.com>
2023-05-03 14:57:37 -0700

    Revert "reworked the client.java n implemented all methods"
    This reverts commit 37398986a060609cbe5abc27996a8bce3b805795.

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -1,88 +1,37 @@
 package com.company;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
-public class BudgetCalendar{
-    private List<Transaction> transactions;
-
-    public BudgetCalendar() {
-        this.transactions = new ArrayList<>();
-    }
-
-    public void addTransaction(Transaction transaction) {
-        this.transactions.add(transaction);
-    }
-
-    public void removeTransactionByName(String name) {
-        Transaction transactionToRemove = null;
-        for (Transaction transaction : transactions) {
-            if (transaction.getName().equals(name)) {
-                transactionToRemove = transaction;
-                break;
-            }
-        }
-        if (transactionToRemove != null) {
-            transactions.remove(transactionToRemove);
-        }
-    }
-
-    public List<Transaction> getTransactions() {
-        return transactions;
-    }
-}
-
-
 public class Client {
-    public static void main(String[] args) {
-        BudgetCalendar budgetCalendar = new BudgetCalendar();
-        Transaction transaction1 = new Transaction("Salary", 5000.0);
-        Transaction transaction2 = new Transaction("Rent", -1200.0);
-        Transaction transaction3 = new Transaction("Groceries", -300.0);
-        budgetCalendar.addTransaction(transaction1);
-        budgetCalendar.addTransaction(transaction2);
-        budgetCalendar.addTransaction(transaction3);
-        budgetCalendar.removeTransactionByName("Rent");
-        List<List<Transaction>> transactionsByMonth = new ArrayList<>();
-        for (int i = 1; i <= 12; i++) {
-            List<Transaction> transactionsInMonth = new ArrayList<>();
-            for (Transaction transaction : budgetCalendar.getTransactions()) {
-                if (transaction.getMonth() == i) {
-                    transactionsInMonth.add(transaction);
-                }
-            }
-            transactionsByMonth.add(transactionsInMonth);
-        }
-        System.out.println("Transactions by month:");
-        for (int i = 0; i < transactionsByMonth.size(); i++) {
-            System.out.println("Month " + (i+1) + ": " + transactionsByMonth.get(i));
-        }
-    }
+    public String name;
+    public int balance;
+    List<Transaction> MonthlyTransactionals = new ArrayList<>();
+
+    Client(String name, int balance) {
+        this.name = name;
+        this.balance = balance;
+
     }
 
-    class Transaction {
-        private String name;
-        private double amount;
-        private int month;
-    
-        public Transaction(String name, double amount) {
-            this.name = name;
-            this.amount = amount;
-            this.month = Calendar.getInstance().get(Calendar.MONTH) + 1;
-        }
-    
-        public String getName() {
-            return name;
-        }
-    
-        public double getAmount() {
-            return amount;
-        }
-    
-        public int getMonth() {
-            return month;
-        }
+    public void addTransaction(Transaction transaction){
+
+        MonthlyTransactionals.add(transaction);
+        balance = (int) (balance + transaction.cash);
+    }
+
+    public void printTransactions(){
+        System.out.println(name+"'s Monthly transactions: \n");
+        MonthlyTransactionals.forEach(System.out::println);
+        System.out.println("The user's balance"+ balance);
+    }
+
+
+    //NEED TO IMPLEMENT A METHOD TO REMOVE A TRANSACTION BASED ON THE TRANSACTION NAME
+    //NEED TO IMPLEMENT A WAY IN CLIENT CLASS FOR A LIST OF LISTS. Ex. Each list should present a month, and that list
+    //should reflect THAT month's transactions.
+
+
 
 
 }
